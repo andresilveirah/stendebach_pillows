4c787287ce571a42ab035f8a60b1ca0c79755976
André Herculano <andresilveirah@gmail.com>
2017-06-05 14:29:24 +0200

    bootstrap redux on index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+
+import { createStore } from 'redux';
+
+import reducer from './reducers';
+
+import App from './components/App';
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
