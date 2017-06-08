876ad225f7bcbde5f915ce30becbce09a95702fc
André Herculano <andresilveirah@gmail.com>
2017-06-08 11:01:15 +0200

    refactor Input to call updateAttributes on componentDidMount

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,14 +3,50 @@ import { connect } from 'react-redux';
 
 import { updateAttribute } from '../actions';
 
-const onChangeHandler = (dispatch) => (event) => dispatch(updateAttribute({
-  attributeName: event.target.name,
-  value: event.target.value
-}));
+class Input extends React.PureComponent {
+  constructor(props) {
+    super(props);
 
-let Input = ({dispatch, ...inputProps}) => (
-  <input {...inputProps} onInput={onChangeHandler(dispatch)} onChange={onChangeHandler(dispatch)} />
-);
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+  }
+
+  isChecked() {
+    return this.isCheckable() && (this.props.checked || this.props.defaultChecked);
+  }
+
+  isCheckable() {
+    return this.props.type === 'checkbox' || this.props.type === 'radio';
+  }
+
+  initialValue() {
+    return this.props.value || this.props.defaultValue;
+  }
+
+  dispatchUpdateAttribute(attributeName, value) {
+    this.props.dispatch(updateAttribute({ attributeName, value }));
+  }
+
+  componentDidMount() {
+    if(this.isCheckable() && !this.isChecked()){ return; }
+
+    this.setInitialValues();
+  }
+
+  setInitialValues() {
+    this.dispatchUpdateAttribute(this.props.name, this.initialValue());
+  }
+
+  onChangeHandler(event) {
+    this.dispatchUpdateAttribute(event.target.name, event.target.value);
+  }
+
+  render() {
+    const { dispatch, ...inputProps} = this.props;
+    return (
+      <input {...inputProps} onInput={this.onChangeHandler} onChange={this.onChangeHandler} />
+    )
+  }
+};
 
 Input = connect()(Input);
 
