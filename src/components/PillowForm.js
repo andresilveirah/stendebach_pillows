37af00b4dce8e2a294200cc064225a70a13f6391
André Herculano <andresilveirah@gmail.com>
2017-06-16 18:40:40 +0200

    implement PillowModal

--- src/components/PillowForm.js
+++ src/components/PillowForm.js
@@ -5,20 +5,50 @@ import BodyMeasurements from './BodyMeasurements';
 import SleepingPosition from './SleepingPosition';
 import MattressType from './MattressType';
 import FormFooter from './FormFooter';
+import PillowModal from './PillowModal';
 
 import { getPillow } from '../reducers';
 
 import '../bootstrap.min.css'
 
-let PillowForm = (props) => (
-  <form action="#" onSubmit={(event) => event.preventDefault()}>
-    <BodyMeasurements />
-    <SleepingPosition />
-    <MattressType />
-    <FormFooter />
-    <span>{props.pillow}</span>
-  </form>
-);
+class PillowForm extends React.PureComponent {
+  constructor() {
+    super();
+    this.state = { showModal: false }
+    this.formSubmitHandler = this.formSubmitHandler.bind(this);
+    this.onCloseModalClick = this.onCloseModalClick.bind(this);
+  }
+
+  formSubmitHandler(event) {
+    event.preventDefault();
+    this.showModal();
+  }
+
+  showModal() {
+    this.setState({ showModal: true });
+  }
+
+  onCloseModalClick(event) {
+    event.preventDefault();
+    this.setState({ showModal: false });
+  }
+
+  render() {
+    return (
+      <form action="#" onSubmit={this.formSubmitHandler}>
+        <BodyMeasurements />
+        <SleepingPosition />
+        <MattressType />
+        <FormFooter />
+        <PillowModal
+          showModal={this.state.showModal}
+          onCloseModalClick={this.onCloseModalClick}
+          pillow={this.props.pillow}
+        />
+      </form>
+    );
+  }
+}
 
 const mapStateToProps = (state) => ({
   pillow: getPillow(state)
