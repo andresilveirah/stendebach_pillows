0e2a46272a8e5105e259ad0bab4d688d2e9fd33c
André Herculano <andresilveirah@gmail.com>
2017-06-16 17:43:28 +0200

    add sleepingReducer to scores reducer

--- src/reducers/scores/index.js
+++ src/reducers/scores/index.js
@@ -1,10 +1,15 @@
 import { combineReducers } from 'redux';
 
 import bodyMeasurements, * as fromBodyMeasurements from './bodyMeasurements';
+import sleepingPosition, * as fromSleepingPosition from './sleepingPosition';
 
 export const totalScore = ({ scores }) =>
-  fromBodyMeasurements.totalScore(scores.bodyMeasurements);
+  fromBodyMeasurements.totalScore(scores.bodyMeasurements) *
+  fromSleepingPosition.totalScore(scores.sleepingPosition);
 
-const scores = combineReducers({ bodyMeasurements });
+const scores = combineReducers({
+  bodyMeasurements,
+  sleepingPosition
+ });
 
 export default scores;
