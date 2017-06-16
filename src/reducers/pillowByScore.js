5eea5d53b526fab20d7304e476f31e67cf79ed16
André Herculano <andresilveirah@gmail.com>
2017-06-16 19:34:35 +0200

    add pillow picture on pillowByScore

--- src/reducers/pillowByScore.js
+++ src/reducers/pillowByScore.js
@@ -3,15 +3,22 @@ import sortBy from 'lodash/fp/sortBy';
 import find from 'lodash/fp/find';
 import thru from 'lodash/fp/thru';
 
+import pillow1 from '../assets/gcozy/GCozy_NSK_orange_8cm.png';
+import pillow2 from '../assets/gcozy/GCozy_NSK_gruen_10cm.png';
+import pillow3 from '../assets/gcozy/GCozy_NSK_blau_13cm.png';
+
+const PILLOWS = [
+  { name: 'Nackenstützkissen G-COZY', height: '8 cm', picture: pillow1 },
+  { name: 'Nackenstützkissen G-COZY', height: '10 cm', picture: pillow2 },
+  { name: 'Nackenstützkissen G-COZY', height: '13 cm', picture: pillow3 }
+];
+
 // These are the score thresholds for each pillow
 // Example: a score of up to 21 corresponds to pillow1, a score of 27 corresponds to pillow2, and so on.
 const SCORE_THRESHOLDS = [
-  { maximumScore: 21, pillow: 'pillow1' },
-  { maximumScore: 36, pillow: 'pillow2' },
-  { maximumScore: 51, pillow: 'pillow3' },
-  { maximumScore: 66, pillow: 'pillow4' },
-  { maximumScore: 71, pillow: 'pillow5' },
-  { maximumScore: 90, pillow: 'pillow6' }
+  { maximumScore: 21, pillow: PILLOWS[0] },
+  { maximumScore: 36, pillow: PILLOWS[1] },
+  { maximumScore: 51, pillow: PILLOWS[2] }
 ];
 
 const pillowByScore = (score, thresholds = SCORE_THRESHOLDS) => {
