eedc3b759779156ca28168c846c3aa759b95a741
André Herculano <andresilveirah@gmail.com>
2017-06-16 17:12:18 +0200

    remove button "Abfrage2"

--- src/components/FormFooter.js
+++ src/components/FormFooter.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-import '../bootstrap.min.css'
+import '../bootstrap.min.css';
 
 const FormFooter = (props) => (
   <div className="row justify-content-center">
     <button type="submit" className="col-sm-4 btn btn-primary">Abfrage 1 &rarr;</button>
-    <div className="col-4"></div>
-    <button type="submit" className="col-sm-4 btn btn-primary">Abfrage 2 &rarr;</button>
   </div>
-)
+);
 
 export default FormFooter;
