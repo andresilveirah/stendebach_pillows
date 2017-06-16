35f2e27354d250bf04fd248885fa36d721bdbeee
André Herculano <andresilveirah@gmail.com>
2017-06-16 18:03:13 +0200

    implement sleepingPosition score in sleepingPosition reducer

--- src/reducers/scores/sleepingPosition.js
+++ src/reducers/scores/sleepingPosition.js
@@ -1,9 +1,20 @@
+const SLEEPING_POSITION_SCORES = {
+  bellyDown: 1,
+  backDown: 1.2,
+  lateralLeaning: 1.3,
+  lateral: 1.5
+};
+
 const sleepingPosition = (state = 1, action) => {
   switch (action.type) {
     case 'UPDATE_ATTRIBUTE':
       // here we are only interested in the attributeName sleepingPosition
       if(action.attributeName === 'sleepingPosition') {
-        return 1;
+        const sleepingPositionScore = SLEEPING_POSITION_SCORES[action.value];
+        if(sleepingPositionScore === undefined) {
+          throw new Error(`could not find a score for ${action.attributeName} with the value of ${action.value}`);
+        }
+        return sleepingPositionScore;
       } else {
         return state;
       }
--- src/reducers/scores/sleepingPosition.spec.js
+++ src/reducers/scores/sleepingPosition.spec.js
@@ -2,18 +2,36 @@ import sleepingPosition, { totalScore } from './sleepingPosition';
 
 let action;
 
+const sleepingPositionScores = {
+  bellyDown: 1,
+  backDown: 1.2,
+  lateralLeaning: 1.3,
+  lateral: 1.5
+};
+
 describe('scores reducer', () => {
   describe('receiving UPDATE_ATTRIBUTE', () => {
     beforeEach(() => { action = { type: 'UPDATE_ATTRIBUTE' } });
 
     describe('when the attributeName is sleepingPosition', () => {
-      beforeEach(() => { action = {...action, attributeName: 'sleepingPosition' }});
+      beforeEach(() => { action = {...action, attributeName: 'sleepingPosition' } });
 
       describe('and value is unknown', () => {
         it('returns 1', () => {
-          expect(sleepingPosition(undefined, action)).toEqual(1);
+          expect(() => { sleepingPosition(undefined, action)})
+            .toThrow(/could not find a score/i);
         });
       });
+
+      Object.keys(sleepingPositionScores).forEach(value => {
+        describe('and value is unknown', () => {
+          beforeEach(() => { action = {...action, value } });
+
+          it(`returns the score of ${sleepingPositionScores[value]}`, () => {
+            expect(sleepingPosition(undefined, action)).toEqual(sleepingPositionScores[value]);
+          });
+        });
+      })
     });
   });
 });
