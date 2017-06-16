6314317f44dfa2b42c7a46dc6296d32c00951801
André Herculano <andresilveirah@gmail.com>
2017-06-16 15:58:27 +0200

    camelcasify components values

--- src/components/BodyMeasurements.js
+++ src/components/BodyMeasurements.js
@@ -7,12 +7,12 @@ import { connect } from 'react-redux';
 
 import '../bootstrap.min.css'
 
-let BodyMeasurements = ({ shoulders, neck, head, ear_to_shoulder }) => (
+let BodyMeasurements = ({ shoulders, neck, head, earToShoulder }) => (
   <Fieldset legend="Körpermaße">
     <RangeInput name='shoulders' label='Schulterabstand' min='27' max='64' value={shoulders} />
     <RangeInput name='neck' label='Halsumfang' min='25' max='49' value={neck} />
     <RangeInput name='head' label='Kopfumfang' min='40' max='79' value={head} />
-    <RangeInput name='ear_to_shoulder' label='Halslänge' min='8' max='27' value={ear_to_shoulder} />
+    <RangeInput name='earToShoulder' label='Halslänge' min='8' max='27' value={earToShoulder} />
   </Fieldset>
 );
 
