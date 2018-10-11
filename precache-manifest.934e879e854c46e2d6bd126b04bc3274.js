7a28674ebcc8335bf03bd0d7632ea4a4441273e7
SemaphoreCiDeployer <andresilveirah@gmail.com>
2018-10-11 09:18:24 +0000

    Updates

---
+++ precache-manifest.934e879e854c46e2d6bd126b04bc3274.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "86a8f88845dfa920b2c23805c40288fc",
+    "url": "/stendebach_pillows/static/media/pillow8.86a8f888.jpg"
+  },
+  {
+    "revision": "41319dbed30f4580b669fc68b28f74d5",
+    "url": "/stendebach_pillows/static/media/pillow13.41319dbe.jpg"
+  },
+  {
+    "revision": "8c9ea88682dd3b1f270b2bd40d9ab01d",
+    "url": "/stendebach_pillows/static/media/pillow10.8c9ea886.jpg"
+  },
+  {
+    "revision": "d1ac8e853532b3850b3662381311123e",
+    "url": "/stendebach_pillows/static/media/G-Cozy_Vermessung.d1ac8e85.png"
+  },
+  {
+    "revision": "45e343449651b8501a30",
+    "url": "/stendebach_pillows/static/js/runtime~main.45e34344.js"
+  },
+  {
+    "revision": "7daac133c84745ee40f7",
+    "url": "/stendebach_pillows/static/js/main.7daac133.chunk.js"
+  },
+  {
+    "revision": "42130774cb5b61e4e0ae",
+    "url": "/stendebach_pillows/static/js/1.42130774.chunk.js"
+  },
+  {
+    "revision": "7daac133c84745ee40f7",
+    "url": "/stendebach_pillows/static/css/main.5f2461f5.chunk.css"
+  },
+  {
+    "revision": "b145d3c218b06168b85d4fac345c3cb3",
+    "url": "/stendebach_pillows/index.html"
+  }
+];
