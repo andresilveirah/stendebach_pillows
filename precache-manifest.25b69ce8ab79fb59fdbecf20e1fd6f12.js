524689ca55144c5ab230857c845cf4286c7f4988
SemaphoreCiDeployer <andresilveirah@gmail.com>
2019-07-05 10:15:57 +0000

    Updates

---
+++ precache-manifest.25b69ce8ab79fb59fdbecf20e1fd6f12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2cda6b12d65d5883fba263a651fb420",
+    "url": "/stendebach_pillows/index.html"
+  },
+  {
+    "revision": "dbe4244abca58bfb4c29",
+    "url": "/stendebach_pillows/static/css/main.e9ebdd34.chunk.css"
+  },
+  {
+    "revision": "719fc3167a2ca28e98fa",
+    "url": "/stendebach_pillows/static/js/2.0ea8f99d.chunk.js"
+  },
+  {
+    "revision": "dbe4244abca58bfb4c29",
+    "url": "/stendebach_pillows/static/js/main.fcaecda0.chunk.js"
+  },
+  {
+    "revision": "4d12d294e086c7ec8185",
+    "url": "/stendebach_pillows/static/js/runtime~main.90f02c26.js"
+  },
+  {
+    "revision": "72628e44fc1e59063f8184f7580e3ff0",
+    "url": "/stendebach_pillows/static/media/loading.72628e44.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/stendebach_pillows/precache-manifest.fc9cf8641a2e119203cbf8f9a7ea20dc.js"
+  "/stendebach_pillows/precache-manifest.25b69ce8ab79fb59fdbecf20e1fd6f12.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/stendebach_pillows/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stendebach_pillows/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
